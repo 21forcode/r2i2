689f95eef77ee461da266b55ca0676bd0b336f40
21forcode <manishlucky413@gmail.com>
2019-07-20 13:01:04 +0530

    Update gsp.py

--- gsp.py
+++ gsp.py
@@ -19,7 +19,7 @@ f =""
 #takes the input of path of the image to be extracted
 f = raw_input("Enter the name of your text file - please use / backslash when typing in directory path: ")
 #Returns the result of a Tesseract OCR run on the image to string in english and hindi 
-p=pytesseract.image_to_string(Image.open(f),lang='eng+hin',config="-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/ -psm 6")
+p=pytesseract.image_to_string(Image.open(f),lang='eng',config="-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/ -psm 6")
 #dump the extracted data to google -spreadsheet API
 s=p.split('\n')
 i=0
@@ -31,6 +31,7 @@ for item in s:
 for item in s:
 	i=i+1
 	sh.update_cell(i,1,item)
+sh.get_all_values
 #print all the records of spreadsheet
 print(sh.get_all_values())
 #create another spreadsheet to store ocr data in organised manner
@@ -39,18 +40,28 @@ osh=gc.open("organize").sheet1
 osh.update_cell(1,1,'Name')
 osh.update_cell(2,1,'Address')
 osh.update_cell(3,1,'DOB')
-
+osh.update_cell(4,1,'Nationality')
+osh.update_cell(5,1,'Birth Place')
 #append name address and date of birth in respective columns
-for item in s:
-	p=item.find('Name',0,5)
-	if p == 0:
-		#print(item[4:])
-		osh.update_cell(1,2,item[4:])
-	if item.find('Add',0,5) == 0:
-		#print(item[3:])
-		osh.update_cell(2,2,item[3:])
-	if item.find('DOB') >= 0:
-		osh.update_cell(3,2,item[(item.find('DOB'))+3:])
+l= len(s);
+for i in range (l):
+	item=s[i]
+	if item.find('Surname')>=0 or s[i].find('Sumame')>=0:
+		print (s[i+1])
+		osh.update_cell(1,3,s[i+1])
+	if item.find('Names')>=0:
+		print(s[i+1])
+		osh.update_cell(1,2,s[i+1])
+	if item.find('INDIAN')>=0:
+		t=s[i].split(' ')
+		print(t[0])
+		osh.update_cell(3,2,t[2])
+		osh.update_cell(4,2,t[0])
+		print(t[2])
+	if item.find('Birth')>=0:
+		print(s[i+1])
+
+		osh.update_cell(5,2,s[i+1])
 		#print(item[(item.find('DOB'))+3:])
 #print all the records of spreadsheet
 for item in osh.get_all_values():
