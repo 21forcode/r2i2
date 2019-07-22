33b0c95b7f7404ae05156a7c8dd921b3ae2d6cc4
21forcode <manishlucky413@gmail.com>
2019-07-22 23:22:25 +0530

    Update gsp.py

--- gsp.py
+++ gsp.py
@@ -1,63 +1,69 @@
-#import needed modules
 import gspread
 import pygsheets
 from oauth2client.service_account import ServiceAccountCredentials
-scope = ['https://spreadsheets.google.com/feeds',
-          'https://www.googleapis.com/auth/drive']
-#check for the credentials of google spreadsheet api
-credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/lucky21/Downloads/manish for R2i2-670ce6e648db.json', scope)
-gc = gspread.authorize(credentials)
-#create a spreadsheet
-
-sh=gc.open("project").sheet1
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+import pandas as pd
+from PIL import Image
 import pytesseract
-f =""
-#takes the input of path of the image to be extracted
-f = raw_input("Enter the name of your text file - please use / backslash when typing in directory path: ")
-#Returns the result of a Tesseract OCR run on the image to string in english and hindi 
-p=pytesseract.image_to_string(Image.open(f),lang='eng',config="-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/ -psm 6")
-#dump the extracted data to google -spreadsheet API
-s=p.split('\n')
-i=0
-for item in s:
-#remove the empty lines 
-	if not item:
-		s.remove(item)
-#dump the ocr data into spreadsheet
-for item in s:
-	i=i+1
-	sh.update_cell(i,1,item)
-sh.get_all_values
-#print all the records of spreadsheet
-print(sh.get_all_values())
-#create another spreadsheet to store ocr data in organised manner
-osh=gc.open("organize").sheet1
-#update the first row with attributes name
-osh.update_cell(1,1,'Name')
-osh.update_cell(2,1,'Address')
-osh.update_cell(3,1,'DOB')
-osh.update_cell(4,1,'Nationality')
-osh.update_cell(5,1,'Birth Place')
-#append name address and date of birth in respective columns
-l= len(s);
-for i in range (l):
-	item=s[i]
-	if item.find('Surname')>=0 or s[i].find('Sumame')>=0:
-		osh.update_cell(1,3,s[i+1])
-	if item.find('Names')>=0:
-		osh.update_cell(1,2,s[i+1])
-	if item.find('INDIAN')>=0:
-		t=s[i].split(' ')
-		osh.update_cell(3,2,t[2])
-		osh.update_cell(4,2,t[0])
-	if item.find('Birth')>=0:
-		osh.update_cell(5,2,s[i+1])
-		
-#print all the records of spreadsheet
-for item in osh.get_all_values():
+def crop(image_path, coords, saved_location):
+	image_obj = Image.open('/home/lucky21/Music/work/img2.jpg')
+	#coords=(311,448,680,690)
+	#print(image_obj.size)
+	im3 = image_obj.resize((800,1079), Image.BILINEAR)
+	cropped_image = im3.crop(coords)
+	#cropped_image.save(saved_location)
+	cropped_image.show()
+	#conductng ocr on cropped image
+	p=pytesseract.image_to_string(cropped_image,lang='eng',config="-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/ -psm 6")
+	s=p.split('\n')
 
-	print(item)
+	for item in s:
+		if item == '':
+			s.remove(item)
+	l= len(s);
+	#or i in range (l-1):
+	return (s[l-1])
+if __name__ == '__main__':
+	image = '/home/lucky21/Music/work/img2.jpg'
+	scope = ['https://spreadsheets.google.com/feeds',
+        	  'https://www.googleapis.com/auth/drive']
+	#check for the credentials of google spreadsheet api
+	credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/lucky21/Downloads/manish for R2i2-670ce6e648db.json', scope)
+	gc = gspread.authorize(credentials)
+	sh=gc.open("project").sheet1
+	#takes the input of path of the image to be extracted
+	#f = raw_input("Enter the name of your text file - please use / backslash when typing in directory path: ")
+	#Returns the result of a Tesseract OCR run on the image to string in english and hindi 
+	p=pytesseract.image_to_string(Image.open(image),lang='eng',config="-c tessedit_char_whitelist=01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/ -psm 6")
+	#dump the extracted data to google -spreadsheet API
+	s=p.split('\n')
+	i=0
+	for item in s:
+	#remove the empty lines 
+		if not item:
+			s.remove(item)
+	#dump the ocr data into spreadsheet
+	i=1
+	for item in s:
+		sh.update_cell(i,1,item)
+		i=i+1
+	print(sh.get_all_values)
+	#create another spreadsheet to store ocr data in organised manner
+	osh=gc.open("organize").sheet1
+	osh.update_cell(1,1,'Name')
+	osh.update_cell(3,1,'DOB')
+	osh.update_cell(5,1,'Birth Place')
+	sname=crop(image, (311,652,448,690), 'cropped.jpg')
+	osh.update_cell(1,3,sname)
+	name=crop(image, (311,697,508,732), 'cropped.jpg')
+	osh.update_cell(1,2,name)
+	dob=crop(image, (593,715,783,782), 'cropped.jpg')
+	osh.update_cell(3,2,dob)
+	pob=crop(image, (353,786,688,822), 'cropped.jpg')
+	osh.update_cell(5,2,pob)
+	#print all the records of spreadsheet
+	for item in osh.get_all_values():
+	
+		print(item)
+
+	
+	
